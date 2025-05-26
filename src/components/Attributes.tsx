57ac1ad4094e8fcb5741ad4265581b871de0640d
Ali-Azimi00 <alidotazimi@gmail.com>
2025-05-26 15:21:14 -0500

    added perception and initative attribute fields

--- src/components/Attributes.tsx
+++ src/components/Attributes.tsx
@@ -29,6 +29,35 @@ const Attributes = () => {
             md:justify-around 
             gap-1'>
                 {renderAttributeFields()}
+
+                <div className='justify-center w-56 text-black'>
+                    <div className='mb-2 bg-gray-400 pl-2 border-1 border-black
+                    border-r-0 flex justify-end rounded-tr-lg' >
+                        <div className='pr-6'>
+                            Passive Perception:
+                            <span className='ml-2'>
+                                00
+                            </span>
+                        </div>
+                        <div className='relative'>
+                            <input type="checkbox" className="absolute border-none top-0 right-0 " />
+                        </div>
+                    </div>
+
+
+                    <div className='mb-2 bg-gray-400 pl-2 border-1 border-black
+                    border-r-0 flex justify-end rounded-tr-lg'>
+                        <div className='pr-6'>
+                            Initiative:
+                            <span className='ml-2'>
+                                00
+                            </span>
+                        </div>
+                        <div className='relative'>
+                            <input type="checkbox" className="absolute border-none top-0 right-0 " />
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
