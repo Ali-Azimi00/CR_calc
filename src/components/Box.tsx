fd14426be64b3e22ac8ea99ba229048fcdf5dc48
Ali-Azimi00 <alidotazimi@gmail.com>
2025-05-31 15:33:51 -0500

    created reusabe box component

---
+++ src/components/Box.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+interface BoxOptions {
+    title: string,
+    value: string,
+    width: string,
+    boxStyle: string | null,
+    titleStyle: string | null,
+    valueStyle: string | null,
+};
+
+
+
+const Box: React.FC<BoxOptions> = ({ title, value, width, boxStyle, titleStyle, valueStyle }) => {
+
+    return (
+        <div className={`${width}`}>
+            <div className={`border-1 border-gray-400 text-center ${boxStyle}`}>
+                <div className={`bg-gray-400 text-sm ${titleStyle}`} >
+                    {title}
+                </div>
+                <div className={`${valueStyle}`}>
+                    {value}
+                </div>
+            </div>
+
+        </div>)
+}
+
+export default Box;
