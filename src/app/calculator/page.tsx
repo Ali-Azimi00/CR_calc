bf821654a7c0b33fe3fff698a507c55cab6f5d48
Ali-Azimi00 <alidotazimi@gmail.com>
2025-06-21 15:11:54 -0500

    added fields to CR tag

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,6 +1,7 @@
 import Attributes from "@/components/Attributes"
 import Offensive from "@/components/Offensive"
 import Defensive from "@/components/Defensive"
+import CrTag from "@/components/CrTag"
 // import Dropdown from "@/components/Dropdown"
 //rafce
 const Calculator = () => {
@@ -9,11 +10,7 @@ const Calculator = () => {
     return (
         <div className="">
 
-            <div className="crTracker ">
-                <div>
-                    CR: ##
-                </div>
-            </div>
+            <CrTag />
 
             <div className="flex flex-wrap justify-center">
                 <div className=" mb-4  justify-center min-w-100 max-w-[1000px] border-2 border-red-800">
